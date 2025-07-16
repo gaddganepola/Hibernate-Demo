0a7512ddafd1ebef35fc48e7d733b78898b224d9
gaddganepola <ganeypola@gmail.com>
2025-07-16 22:28:30 +0530

    ehcache demo

---
+++ src/main/java/org/example/ehcache/Main.java
@@ -0,0 +1,29 @@
+package org.example.ehcache;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+    public static void main(String[] args) {
+        Configuration config = new Configuration();
+        config.addAnnotatedClass(org.example.ehcache.Student.class);
+        config.configure();
+
+        SessionFactory sf = config.buildSessionFactory();
+        Session session = sf.openSession();
+
+        Student s1 = session.byId(Student.class).load(3);
+        System.out.println(s1);
+
+        session.close();
+
+        Session session2 = sf.openSession();
+        Student s2 = session2.byId(Student.class).load(3);
+        System.out.println(s2);
+
+        session2.close();
+
+        sf.close();
+    }
+}
